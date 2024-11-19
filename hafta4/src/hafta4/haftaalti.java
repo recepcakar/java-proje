aa9e777552f9084c94726a1d19f1cd574039158a
recep <rrecepcakar@gmail.com>
2024-11-19 13:09:20 +0300

    a

---
+++ hafta4/src/hafta4/haftaalti.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+package hafta4;
+
+public class haftaalti 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList<String> car =new ArrayList<String>();  
+       car.add("B M W");
+       car.add("DOBLO");
+       car.add("COROLLA");
+       car.add("JAGUAR");
+       for(String ncar : car) {
+    	   System.out.println(ncar);
+       }
+       
+	}
+
+}
