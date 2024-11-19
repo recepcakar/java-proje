f021af7c6694f14d478734618e413e56311feeae
recep <rrecepcakar@gmail.com>
2024-11-19 13:07:58 +0300

    javaders

---
+++ haftaseki/src/haftaseki/haftasekiz.java
@@ -0,0 +1,10 @@
+package haftaseki;
+
+public class haftasekiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- haftaseki/src/module-info.java
+++ haftaseki/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module hafta4 {
+module haftaseki {
 }
